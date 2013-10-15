c886e59eb67418ec5692ce26abba705a35dee1ad
Matvey Kruglov <kubuzzzz@gmail.com>
2013-10-15 11:40:46 +0400

    hh-main: Read projects paths from config file

---
+++ hh-main/config.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+CONFIG = {
+
+    "hh-stubs-root" => "/tmp/hh-stubs",
+    "hh-sites-main-root" => "/tmp/hh-sites-main",
+    "hh-sites-common-root" => "/tmp/hh-sites-common",
+    "hh-cms-root" => "/tmp/hh-cms",
+    "hh-php-app-root" => "/tmp/hh-php-app",
+    "hh-php-packages-root" => "/tmp/hh-php-packages-root",
+}
